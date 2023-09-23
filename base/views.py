d8774403fbcab5991769366d45a0dbcc02f33a27
purnesh <48253358+purneshkotian@users.noreply.github.com>
2023-09-23 17:49:56 +0530

    theme changess

--- base/views.py
+++ base/views.py
@@ -105,6 +105,7 @@ def userProfile(request, pk):
 @login_required(login_url='base/login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
@@ -113,7 +114,7 @@ def createRoom(request):
             form.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -121,6 +122,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You\'re not allowed to update')
@@ -131,7 +133,7 @@ def updateRoom(request, pk):
             form.save()
             return redirect('home')
 
-    context = {'form': form}
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
