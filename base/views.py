3d277ee1d6ccdc4a18528fccba305ea5b698a1d3
purnesh <48253358+purneshkotian@users.noreply.github.com>
2023-09-24 15:18:38 +0530

    register form setup

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -177,5 +177,13 @@ def deleteMessage(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-    context = None
+    user = request.user
+    form = UserForm(instance=user)
+    context = {'form': form}
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
     return render(request, 'base/update_user.html', context)
